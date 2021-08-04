3c1758bf53dadb642730571dd1786817273969c0
Quocanh <phiquocanh717@gmail.com>
2021-08-04 10:36:04 +0700

    Initial commit

---
+++ src/main/java/com/codegym/demo/controller.java
@@ -0,0 +1,13 @@
+package com.codegym.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class controller {
+    @GetMapping("/greeting")
+    public ModelAndView showGreeting(){
+        return new ModelAndView("/greeting");
+    }
+}
